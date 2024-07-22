a83305f08aa3972bbc3adc3668dda0e49b3f5fdc
IthavinduU <thavindul@gmail.com>
2024-07-22 15:19:01 +0530

    Minor updates

---
+++ components/Buttons.js
@@ -0,0 +1,50 @@
+// components/ResumeButton.js
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const ResumeButton = () => {
+  return (
+    <div className="flex justify-start items-center space-x-4">
+      {/* View Resume Button */}
+      <Link
+        href="/Thavindu Liyanage CV.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <motion.button
+          className="bg-accent text-white px-6 py-3 rounded-lg shadow-lg font-semibold cursor-pointer"
+          whileHover={{
+            scale: 1.05,
+            boxShadow: "0 10px 20px rgba(0,0,0,0.2)",
+            backgroundColor: "#ff5722",
+          }}
+          whileTap={{ scale: 0.95 }}
+        >
+          View Resume
+        </motion.button>
+      </Link>
+
+      {/*---------------------------------------------------------------------*/}
+      {/* Contact Me Button */}
+      <Link
+        href="mailto:thavindul@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <motion.button
+          className="bg-accent text-white px-6 py-3 rounded-lg shadow-lg font-semibold cursor-pointer"
+          whileHover={{
+            scale: 1.05,
+            boxShadow: "0 10px 20px rgba(0,0,0,0.2)",
+            backgroundColor: "#ff5722",
+          }}
+          whileTap={{ scale: 0.95 }}
+        >
+          Contact Me
+        </motion.button>
+      </Link>
+    </div>
+  );
+};
+
+export default ResumeButton;
