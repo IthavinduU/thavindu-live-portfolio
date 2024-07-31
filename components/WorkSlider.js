70d6515608743f986ad5fda0a8159d13add6f889
IthavinduU <thavindul@gmail.com>
2024-07-31 21:42:22 +0530

    Work page

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -42,7 +42,7 @@ const WorkSlider = () => {
                         <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                         {/* title */}
                         <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
-                          <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          <div className="flex items-center gap-x-2 text-[10px] sm:text-[12px] md:text-[13px] xl:text-[14px] tracking-[0.2em]">
                             {/* project title */}
                             <div className="delay-100">{image.title}</div>
                             {/* icon */}
--- pages/work/index.js
+++ pages/work/index.js
@@ -3,21 +3,22 @@ import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
+import Footer from "../../components/Footer";
 
 const Work = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-8 xl:mb-0">
             <motion.h2
               variants={fadeIn("up", 0.2)}
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="h2 xl:mt-12"
+              className="text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-semibold xl:mt-12 mb-4"
             >
               Projects <span className="text-accent">.</span>
             </motion.h2>
@@ -26,9 +27,9 @@ const Work = () => {
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+              className="mb-8 max-w-[400px] mx-auto lg:mx-0 text-sm sm:text-base"
             >
-              <p>These are some of the projects I have done.</p>
+              These are some of the projects I have done.
             </motion.p>
           </div>
 
@@ -45,6 +46,8 @@ const Work = () => {
         </div>
       </div>
       <Bulb />
+      {/* Footer */}
+      <Footer />
     </div>
   );
 };
